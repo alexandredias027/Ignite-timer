8c9ef22f318ad0d70b473b516f447afeb9a17bd9
Alexandre Dias <alexandredias027@gmail.com>
2025-03-10 23:24:42 -0300

    feat: Utilizando o context e formatado o inicio do ciclo

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export function History() {
     const { cycles } = useContext(CyclesContext);
@@ -21,44 +23,31 @@ export function History() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>Estudar React</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
+                        {cycles.map(cycle => {
+                            return (
+                                <tr key={cycle.id}>
+                                    <td>{cycle.task}</td>
+                                    <td>{cycle.minutesAmount} minutos</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR
+                                    })}</td>
+                                    <td>
+                                        {cycle.finishedDate && (
+                                            <Status statusColor="green">Finalizado</Status>
+                                        )}
+                                        {cycle.interruptedDate && (
+                                            <Status statusColor="red">Interrompido</Status>
+                                        )}
 
-                        <tr>
-                            <td>Estudar React</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr><tr>
-                            <td>Estudar React</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="yellow">Em andamento</Status>
-                            </td>
-                        </tr><tr>
-                            <td>Estudar React</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="red">Interrompido</Status>
-                            </td>
-                        </tr><tr>
-                            <td>Estudar React</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
+                                        {!cycle.finishedDate && !cycle.interruptedDate && (
+                                            <Status statusColor="yellow">Em andamento</Status>
+                                        )}
+                                    </td>
+                                </tr>
+                            )
+                        })}
+
                     </tbody>
                 </table>
 
